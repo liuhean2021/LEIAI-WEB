061830541a203c2cd31894db778782bc4e4290f1
liuhean <1732440405@qq.com>
2023-09-05 20:16:45 +0800

    feat: 添加v2.9.0版本项目代码
    Signed-off-by: liuhean <1732440405@qq.com>

---
+++ leiai-v2.9.0/dist/modules/chatgpt/dto/addKey.dto.d.ts
@@ -0,0 +1,12 @@
+export declare class AddKeyDto {
+    manufacturer: string;
+    key: string;
+    secretKey: string;
+    status: number;
+    model: string;
+    type: string;
+    weight: number;
+    maxModelTokens: number;
+    maxResponseTokens: number;
+    openaiProxyUrl: string;
+}
